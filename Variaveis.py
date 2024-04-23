50d96e97927aaa6c96e5075cfe8263ba85d08803
FelipeCumba007 <felipecumbapereirafreitas12@gmail.com>
2024-04-23 16:37:01 -0300

    criação variaveis.py

---
+++ Variaveis.py
@@ -0,0 +1,12 @@
+nome="Felipe Cumba"
+idade=16 
+email="felipecumbapereirafreitas12@gmail.com"
+telefone="17 997266723"
+nota=8.9
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
