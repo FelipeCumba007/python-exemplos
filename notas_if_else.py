990dbbd218e863bd72034f53cfe3bc07ee260612
FelipeCumba007 <felipecumbapereirafreitas12@gmail.com>
2024-05-07 14:36:22 -0300

    criação notas_if_else.py

---
+++ notas_if_else.py
@@ -0,0 +1,10 @@
+nota1 = int(input("Informe a nota do 1° Bimestre  (0-100):"))
+nota2 = int(input("Informe a nota do 1° Bimestre  (0-100):"))
+media = (nota1 + nota2) / 2
+if media >= 60:
+    print(f"Você foi aprovado com a média {media}") 
+else:
+    print(f"Você não foi aprovado, sua média é {media}")    
+
+
+
