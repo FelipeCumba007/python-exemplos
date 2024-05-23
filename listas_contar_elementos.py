710884c91fda965c2a4ff0ad94399ff3492afa35
FelipeCumba007 <felipecumbapereirafreitas12@gmail.com>
2024-05-23 10:34:51 -0300

    Update listas_contar_elementos.py

--- listas_contar_elementos.py
+++ listas_contar_elementos.py
@@ -1,4 +1,4 @@
 alunos=["Adriana" , "Jõao" , "Maria" , "Pedro", "Ana" , "Lucas" , "Gabriel", "Mateus", "Adriana"]
 
 ocorrencias = alunos.count("Adriana")
-print(f"Quantidade de ocorrências de Adriana: {ocorrencias}")
+print(f"Quantidade de ocorrências de Adriana: {ocorrencias}") 
