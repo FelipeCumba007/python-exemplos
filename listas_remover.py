fa50a1bf7f2766a0d4dd4cf6065e9b321e3ab59f
FelipeCumba007 <felipecumbapereirafreitas12@gmail.com>
2024-05-23 10:22:37 -0300

    listas_comparar.py

---
+++ listas_remover.py
@@ -0,0 +1,9 @@
+alunos=["Adriana" , "Jõao" , "Maria" , "Pedro", "Ana" , "Lucas" , "Gabriel", "Mateus" ]
+print(f"Listas de alunos: {alunos}")
+# Método remove 
+alunos.remove("Adriana")
+print(f"Lista de alunos: {alunos}")
+# Método pop()
+alunos.pop(2)
+print(f"Lista de alunos: {alunos}")
+# Função del
