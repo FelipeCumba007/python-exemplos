1e87375004744a8949d613d6ecf0f6c681c1ed9c
FelipeCumba007 <felipecumbapereirafreitas12@gmail.com>
2024-05-23 09:07:56 -0300

    Criação listas_ordenação.py

---
+++ listas_ordenação.py
@@ -0,0 +1,12 @@
+import random
+alunos=["Jõao" , "Felipe", "Pedro" , "Ana" , "Lucas" , "Maria"]
+print(f"Lista: {alunos}")
+# Embaralhar a lista
+random.shuffle(alunos)
+print(f"Lista embaralhada: {alunos}")
+# Ordena a Lista Crescente
+alunos.sort()
+print(f"Lista cresente: {alunos}")
+# Ordena a Lista Decrescente
+alunos.sort(reverse=True)
+print(f"Lista decrescente: {alunos}")
